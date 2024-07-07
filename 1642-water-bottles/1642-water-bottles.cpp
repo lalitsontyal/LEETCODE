f392ae1187c5c809d5656a3b0e9203a28551286a
LALIT SINGH <121187566+lalitsontyal@users.noreply.github.com>
2024-07-07 13:03:24 +0530

    Time: 0 ms (100%), Space: 7.2 MB (30.43%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
        int count = numBottles;
-       while(numBottles >= numExchange)
+       while(numBottles - numExchange >= 0)
        {
             count += 1;
             numBottles = numBottles - numExchange;
