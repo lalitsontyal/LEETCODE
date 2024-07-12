8e7d9b4c5b6fd6224e897ea49dbd22508c3ab056
LALIT SINGH <121187566+lalitsontyal@users.noreply.github.com>
2024-07-12 16:15:07 +0530

    Time: 21 ms (41.09%), Space: 24.9 MB (14.21%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -17,11 +17,8 @@ public:
             mid = (lb+ub)/2;
             if(nums[mid] != nums[mid+1]  && nums[mid] != nums[mid-1])
                 return nums[mid];
-            else if(mid%2==0 && nums[mid]==nums[mid+1])
-                lb=mid+1;
-            else if(mid%2==0 && nums[mid] == nums[mid-1])
-                ub=mid-1;
-            else if(mid%2==1 && nums[mid]==nums[mid-1])
+            else if( (mid%2==0 && nums[mid]==nums[mid+1]) ||
+                     (mid%2==1 && nums[mid]==nums[mid-1]))
                 lb=mid+1;
             else
                 ub=mid-1;
