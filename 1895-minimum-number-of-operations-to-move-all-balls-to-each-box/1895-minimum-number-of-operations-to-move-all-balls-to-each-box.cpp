170ef1ee877f1269651b72e97b367fcfffe5224d
LALIT SINGH <121187566+lalitsontyal@users.noreply.github.com>
2025-01-06 20:31:52 +0530

    Time: 0 ms (100%), Space: 12.1 MB (50.97%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
-        vector<int> sol(n, 0); // Initialize sol with size n and all elements as 0
+        vector<int> sol(n, 0); 
         int next = 0, back = 0;
 
-        // First pass: calculate for the first box and count `next`
         for (int i = 0; i < n; i++) {
             if (boxes[i] == '1') {
                 next++;
-                sol[0] += i; // Calculate distance from all '1's to the first box
-            }
+                sol[0] += i; }
         }
 
-        // Adjust `next` and `back` for the first box
         if (boxes[0] == '1') {
             next--;
             back++;
         }
 
-        // Second pass: calculate for the rest of the boxes
         for (int i = 1; i < n; i++) {
-            sol[i] = sol[i - 1] - next + back; // Transition from previous result
+            sol[i] = sol[i - 1] - next + back;
             if (boxes[i] == '1') {
                 next--;
                 back++;
